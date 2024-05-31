c9651b3020eecf9195c0c5b0e798df1f5c5fea45
akorli13 <dakorli004@st.ug.edu.gh>
2024-05-31 16:03:07 +0000

    environment setup

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+import Search from './components/Search';
+import Categories from './components/Categories';
+import OngoingTask from './components/OngoingTask';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header/> 
+      <Search/> 
+      <Categories/>
+      <OngoingTask/>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F0E8',
+  },
+});
